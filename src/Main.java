aa8a58b03b410d5365e09cc7ca81dc54b963ccaa
moves <movesnues@gmail.com>
2024-03-06 16:03:05 +0300

    Initial commits

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,28 @@ public class Main {
     public static void main(String[] args) {
 
 //Задачи 1, 2
-        int[] weight = {1, 2, 3};
+        int[] weight = new int[3];
+        weight[0] = 1;
+        weight[1] = 2;
+        weight[2] = 3;
         double[] number = {1.57, 7.654, 9.986};
         int[] apples = {5, 7, 56, 64, 23, 77};
         for (int i = 0; i < weight.length; i = i + 1) {
-            if (i == 2) {
+            if (i == weight.length - 1) {
                 System.out.println(weight[i]);
                 break;
             }
             System.out.print(weight[i] + ", ");
         }
         for (int i = 0; i < number.length; i = i + 1) {
-            if (i == 2) {
+            if (i == number.length - 1) {
                 System.out.println(number[i]);
                 break;
             }
             System.out.print(number[i] + ", ");
         }
         for (int i = 0; i < apples.length; i = i + 1) {
-            if (i == 5) {
+            if (i == apples.length) {
                 System.out.println(apples[i]);
                 break;
             }
@@ -31,30 +34,36 @@ public class Main {
 
 
 //Задача 3
-        for (int i = 2; i >= 0; i = i - 1) {
+        for (int i = weight.length - 1; i >= 0; i = i - 1) {
             if (i == 0) {
                 System.out.println(weight[i]);
                 break;
             }
             System.out.print(weight[i] + ", ");
         }
-        for (int i = 2; i >= 0; i = i - 1) {
+        for (int i = number.length - 1; i >= 0; i = i - 1) {
             if (i == 0) {
                 System.out.println(number[i]);
                 break;
             }
             System.out.print(number[i] + ", ");
         }
-
+        for (int i = apples.length - 1; i >= 0; i = i - 1) {
+            if (i == 0) {
+                System.out.println(apples[i]);
+                break;
+            }
+            System.out.print(apples[i] + ", ");
+        }
 
 
 //Задача 4
-        int[] a = {1, 2, 3};
-        for (int b = 0; b < a.length; b = b + 1) {
-            if (a[b] % 2 != 0) {
-                a[b] = a[b] + 1;
-                System.out.print(a[b]); }
-            System.out.print(Arrays.toString(a));
+        for (int i = 0; i < weight.length; i = i + 1) {
+            if (weight[i] % 2 != 0) {
+                weight[i] = weight[i] + 1; }
+            System.out.println(Arrays.toString(weight));
+
+
         }
     }
 }
